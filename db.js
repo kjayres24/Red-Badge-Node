7a2923cbb0f47b0bffdeeed31d0d187984aa4ee2
Katie Ayres <katieayres24@gmail.com>
2019-05-20 10:01:22 -0400

    heroku deployment

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize('group-project', 'postgres', 'reggie44', { //change password back to reggie44 before pushing to github
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
